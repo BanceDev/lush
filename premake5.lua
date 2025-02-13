2b1f666ca530fea3876c9d6a6c3ef916f9b73993
BanceDev <lanceaborden@protonmail.com>
2025-02-13 11:59:31 -0500

    v0.3.2

--- premake5.lua
+++ premake5.lua
@@ -27,7 +27,7 @@ files({
 	"lib/hashmap/**.h",
 	"lib/hashmap/**.c",
 })
-defines({ 'LUSH_VERSION="0.3.1"' })
+defines({ 'LUSH_VERSION="0.3.2"' })
 
 filter("configurations:Debug")
 defines({ "DEBUG" })
